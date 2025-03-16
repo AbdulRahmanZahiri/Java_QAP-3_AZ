0fd2d0e46be4655ee130a25f2109da3c973d90ba
Abdul Rahman Zahiri <abdulrahmanzahiri6644@gmail.com>
2025-03-16 02:15:40 -0230

    Screenshot of demo running problem 2 demo

--- Problem2/Demo.java
+++ Problem2/Demo.java
@@ -3,7 +3,7 @@ package Problem2;
 public class Demo {
     public static void main(String[] args) {
         MovablePoint movablePoint = new MovablePoint(1.5f, 2.5f, 0.2f, 0.2f); 
-        System.out.println("Initial " + movablePoint + "\n");
+        System.out.println("Starting " + movablePoint + "\n");
 
         movablePoint.move();
         System.out.println("After moving: " + movablePoint + "\n");
